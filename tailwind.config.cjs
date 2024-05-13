f9af9e1b2fef4d5487ac47155af01323b89030c3
adriandonato19 <adriandonato9@gmail.com>
2024-05-13 16:46:43 -0500

    cambios diversos en colores de areas de practica, iconos y css global, se agrego nuevo color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,6 +11,7 @@ module.exports = {
         accent: 'var(--aw-color-accent)',
         default: 'var(--aw-color-text-default)',
         muted: 'var(--aw-color-text-muted)',
+        'focus-blue': '#04045c', // Add your custom color here
       },
       fontFamily: {
         sans: ['var(--aw-font-sans, ui-sans-serif)', ...defaultTheme.fontFamily.sans],
